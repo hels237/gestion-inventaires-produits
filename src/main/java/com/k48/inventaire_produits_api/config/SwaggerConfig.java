bf65a2c5f79c25fc3b4dfb44b535b07394059a8b
hels <m7hels@gmail.com>
2025-09-01 16:01:57 +0100

    added swagger configuration

---
+++ src/main/java/com/k48/inventaire_produits_api/config/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package com.k48.inventaire_produits_api.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Inventory Management API")
+                        .version("1.0")
+                        .description("API RESTful pour la gestion d'inventaire de produits"));
+    }
+}
